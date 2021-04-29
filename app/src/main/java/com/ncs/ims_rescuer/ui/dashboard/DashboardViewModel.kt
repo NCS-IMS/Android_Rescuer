8fedede7f4c954d86bf776d082d93fc8c597dc9c
Deuk_Hoi <dh97k@naver.com>
2021-04-29 11:17:19 +0900

    init setting

---
+++ app/src/main/java/com/ncs/ims_rescuer/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.ncs.ims_rescuer.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
